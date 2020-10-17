adde hopfield. Fix tuesday main mean calculation

main_tuesday.py
@@ -13,6 +13,8 @@ parser = DataParser('europe.csv')
 # matrix_for_correlation = np.array(parser.get_numerical_csv(), dtype=float).T
 matrix_for_correlation = np.array(parser.get_standarized_matrix())
 
+print('MEDIA (Tiene que tender a 0):', np.mean(matrix_for_correlation), '| STD (Tiene que tender a 1):', np.std(matrix_for_correlation))
+
 # print('ESTANDARIZADA',parser.get_standarized_matrix())
 
 # 'Area','GDP','Inflation','Life.expect','Military','Pop.growth','Unemployment'
@@ -31,22 +33,20 @@ df = pd.DataFrame(matrix_for_correlation_with_keys, columns=[
 
 correlation_matrix = df.corr()
 # If you want to show heatmap:
-sn.heatmap(correlation_matrix, annot=True)
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
 
-covariance_matrix = np.cov(matrix_for_correlation)
+
+covariance_matrix = np.cov(np.array(parser.get_numerical_csv()).T)
+print('COV', covariance_matrix)
 
 autovals_corr, autovecs_corr = np.linalg.eig(correlation_matrix)
+
+print('AUTOVALS | AUTOVECS, COR')
+print(autovals_corr, autovecs_corr)
+
 autovals_cov, autovecs_cov = np.linalg.eig(covariance_matrix)
 
-# print('COR')
-# print(autovals_corr, autovecs_corr)
-
-# print('COV')
-# print(autovals_cov, autovecs_cov)
-
+print('AUTOVALS | AUTOVECS, COV')
+print(autovals_cov, autovecs_cov)
 
 # PCA
 n_components = 7
@@ -59,4 +59,12 @@ print('Cargas de las componentes')
 print(pca.explained_variance_ratio_)
 
 print('Cargas de primer componente principal')
-print(pca.components_[0])
+print(pca.components_)
+
+print('PCA', X_new)
+print('PCA Ratio:', pca.explained_variance_ratio_)
+
+sn.heatmap(correlation_matrix, annot=True)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()